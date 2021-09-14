01e0223c9cda02a841ed4a7020091f670a362973
Ali Saeidi <alisaeidi.windows10@gmail.com>
2021-09-14 22:26:48 +0430

    B-Airport-C2-Get All Airport For Admin

---
+++ FlyWithUs/Areas/Admin/Controllers/AirportsController.cs
@@ -0,0 +1,25 @@
+﻿using FlyWithUs.Hosted.Service.ApplicationService.Services.World;
+using FlyWithUs.Hosted.Service.DTOs.Airports;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyWithUs.Hosted.Service.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AirportsController : Controller
+    {
+        private readonly AirportService airportService;
+        public AirportsController()
+        {
+            airportService = new AirportService();
+        }
+        public IActionResult GetAllAirport()
+        {
+            List<AirportDTO> dtos = airportService.GetAllAirport();
+            return View(dtos);
+        }
+    }
+}
