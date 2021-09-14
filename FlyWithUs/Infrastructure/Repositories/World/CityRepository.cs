a41bba10353b9d768c430cfd56a60ed380a003e3
Ali Saeidi <alisaeidi.windows10@gmail.com>
2021-09-14 23:24:57 +0430

    B-Airport-C7-Get All Airport For Country In Admin Panel

--- FlyWithUs/Infrastructure/Repositories/World/CityRepository.cs
+++ FlyWithUs/Infrastructure/Repositories/World/CityRepository.cs
@@ -33,7 +33,7 @@ namespace FlyWithUs.Hosted.Service.Infrastructure.Repositories.World
 
         public List<City> GetAllCity()
         {
-            return context.Cities.Include(c => c.Country).ToList();
+            return context.Cities.Include(c => c.Country).Include(c => c.Airports).ToList();
         }
 
         public City GetCityById(int cityid)
