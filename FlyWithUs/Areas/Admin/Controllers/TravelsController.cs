e5425a480ddd4f795f3ac53ce895f772aaeac959
Ali Saeidi <alisaeidi.windows10@gmail.com>
2021-09-15 21:30:30 +0430

    B-Travel-C2-Get All Travel For Admin

---
+++ FlyWithUs/Areas/Admin/Controllers/TravelsController.cs
@@ -0,0 +1,25 @@
+﻿using FlyWithUs.Hosted.Service.ApplicationService.Services.Travels;
+using FlyWithUs.Hosted.Service.DTOs.Travels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlyWithUs.Hosted.Service.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TravelsController : Controller
+    {
+        private readonly TravelService travelService;
+        public TravelsController()
+        {
+            travelService = new TravelService();
+        }
+        public IActionResult GetAllTravel()
+        {
+            List<TravelDTO> dtos = travelService.GetAllTravel();
+            return View(dtos);
+        }
+    }
+}
