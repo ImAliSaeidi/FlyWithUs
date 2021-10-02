7d320f6248b17932e53f879a78c17dbab62cfb34
Ali Saeidi <alisaeidi.windows10@gmail.com>
2021-10-02 13:01:21 +0330

    B-Master-Panel-Home

---
+++ FlyWithUs/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlyWithUs.Hosted.Service.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Area("Admin")]
+        [Route("Admin/[Controller]/[Action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
