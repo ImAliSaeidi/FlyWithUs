8168dc3daf375b0644ee1a1f2b22ddf562a1c61c
Ali Saeidi <alisaeidi.windows10@gmail.com>
2021-10-02 21:48:53 +0330

    B-API-User-Validate Token

---
+++ FlyWithUs/Filter/SecurityFilter.cs
@@ -0,0 +1,47 @@
+﻿using FlyWithUs.Hosted.Service.Tools.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Linq;
+using System.Net;
+
+namespace FlyWithUs.Hosted.Service.Filter
+{
+    public class SecurityFilter : ActionFilterAttribute
+    {
+        private readonly string role;
+        public SecurityFilter(string role)
+        {
+            this.role = role;
+        }
+
+        private readonly UserManager<IdentityUser> userManager;
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            bool isAccess = false;
+            base.OnActionExecuting(context);
+
+            var authorization = context.HttpContext.Request.Headers["Authorization"].ToString();
+            if (!string.IsNullOrWhiteSpace(authorization))
+            {
+                if (authorization.Contains("Bearer "))
+                {
+                    var token = authorization.Replace("Bearer ", "");
+                    var claimsPrincipal = TokenGenerator.Validate(token);
+                    var id = claimsPrincipal.Claims.FirstOrDefault(c => c.Type == "UserId").Value;
+                    isAccess = true;
+                }
+            }
+            if (isAccess == false)
+            {
+                UnAhutorize(context);
+            }
+        }
+
+        private void UnAhutorize(ActionExecutingContext context)
+        {
+            var result = new JsonResult(new { Message = "دسترسی مجاز نیست" }) { StatusCode = (int)HttpStatusCode.Unauthorized, ContentType = "application/json" };
+            context.Result = result;
+        }
+    }
+}
